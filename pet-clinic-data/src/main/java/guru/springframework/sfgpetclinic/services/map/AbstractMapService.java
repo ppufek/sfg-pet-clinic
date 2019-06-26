c23ab0f21969255a4a852a36a8d2b1483a6da57a
epufpau <paula.pufek@ericsson.com>
2019-06-26 16:11:28 +0200

    Implemented Map impl for Visit Service. Closes #46

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,10 +1,16 @@
 package guru.springframework.sfgpetclinic.services.map;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
 import guru.springframework.sfgpetclinic.model.BaseEntity;
 
-import java.util.*;
-
 public abstract class AbstractMapService<T extends BaseEntity, ID extends Long>{
+
     protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
