f3df9fc287b8bfaf98dbe1fc209beae2822b7168
epufpau <paula.pufek@ericsson.com>
2019-07-04 13:24:46 +0200

    add ability to add or update pet, but still not working. #56

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -12,10 +12,12 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.validation.Valid;
 import java.util.List;
 
+/**
+ * Created by jt on 7/22/18.
+ */
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
-
     private static final String VIEWS_OWNER_CREATE_OR_UPDATE_FORM = "owners/createOrUpdateOwnerForm";
 
     private final OwnerService ownerService;
@@ -24,38 +26,38 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-//    @RequestMapping({"", "/", "/index", "/index.html"})
-//    public String listOwners(Model model) {
-//        model.addAttribute("owners", ownerService.findAll());
-//        return "owners/index";
-//    }
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
 
     @RequestMapping("/find")
-    public String findOwners(Model model) {
+    public String findOwners(Model model){
         model.addAttribute("owner", Owner.builder().build());
         return "owners/findOwners";
     }
 
     @GetMapping
-    public String processFindForm(Owner owner, BindingResult result, Model model) {
-        //allow parameterless GET request for /owners to return all records
-        if(owner.getLastName() == null) {
-            owner.setLastName(""); //empty string signifies broadest possible search
+    public String processFindForm(Owner owner, BindingResult result, Model model){
+        // allow parameterless GET request for /owners to return all records
+        if (owner.getLastName() == null) {
+            owner.setLastName(""); // empty string signifies broadest possible search
         }
 
-        //find owners by last name
-        List<Owner> results = ownerService.findAllByLastNameLike("%" + owner.getLastName() + "%");
+        // find owners by last name
+        List<Owner> results = ownerService.findAllByLastNameLike("%"+ owner.getLastName() + "%");
 
-        if(results.isEmpty()) {
-            //no owners found
+        if (results.isEmpty()) {
+            // no owners found
             result.rejectValue("lastName", "notFound", "not found");
             return "owners/findOwners";
-        }else if(results.size() == 1) {
-            //1 owner found
+        } else if (results.size() == 1) {
+            // 1 owner found
             owner = results.get(0);
             return "redirect:/owners/" + owner.getId();
-        }else {
-            //multiple owners found
+        } else {
+            // multiple owners found
             model.addAttribute("selections", results);
             return "owners/ownersList";
         }
@@ -76,10 +78,10 @@ public class OwnerController {
 
     @PostMapping("/new")
     public String processCreationForm(@Valid Owner owner, BindingResult result) {
-        if(result.hasErrors()) {
+        if (result.hasErrors()) {
             return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
-        }else {
-            Owner savedOwner = ownerService.save(owner);
+        } else {
+            Owner savedOwner =  ownerService.save(owner);
             return "redirect:/owners/" + savedOwner.getId();
         }
     }
@@ -99,12 +101,5 @@ public class OwnerController {
             Owner savedOwner = ownerService.save(owner);
             return "redirect:/owners/" + savedOwner.getId();
         }
-    }
-
-
-    //security
-    @InitBinder
-    public void setAllowedFields(WebDataBinder dataBinder) {
-        dataBinder.setDisallowedFields("id");
     }
 }
