17d805eda6ee99d5b38b66cf46983622e9fb6e28
epufpau <paula.pufek@ericsson.com>
2019-07-05 14:29:55 +0200

    Added Visit Controller Tests. Closes #65

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VisitController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VisitController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
 import java.util.Map;
 
 @Controller
@@ -24,8 +26,16 @@ public class VisitController {
     }
 
     @InitBinder
-    public void setAllowedFields(WebDataBinder dataBinder) {
+    public void dataBinder(WebDataBinder dataBinder) {
+
         dataBinder.setDisallowedFields("id");
+
+        dataBinder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException{
+                setValue(LocalDate.parse(text));
+            }
+        });
     }
 
     @ModelAttribute("visit")
@@ -45,7 +55,7 @@ public class VisitController {
     }
 
     //Spring MVC calls method loadPetWithVisit(...) before processNewVisitForm is called
-    @PostMapping("owners/{ownedId}/pets/{petId}/visits/new")
+    @PostMapping("owners/{ownerId}/pets/{petId}/visits/new")
     public String processNewVisitForm(@Valid Visit visit, BindingResult result) {
         if(result.hasErrors()) {
             return "pets/createOrUpdateVisitForm";
